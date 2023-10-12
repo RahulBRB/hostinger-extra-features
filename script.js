17c24030f8c7d035127feffa94fde6e50d9847a6
RahulBRB <rahulblue17@gmail.com>
2023-10-12 18:50:33 +0530

    Dark mode + Coupon functionality added

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+window.onload = () => {
+    onLoadPage();
+}
+
+const closeCoupon = () => {
+    const couponElement = document.getElementsByClassName('coupon')[0];
+    couponElement.style.visibility = 'hidden';
+}
+
+const onLoadPage = () =>{
+    const couponBody = getDocumentsByClassName('coupon')[0];
+    couponBody.style.visibility = 'visible'
+}
+
 const mode = () => {
     let newbody = document.body;
     newbody.classList.toggle('dark');
